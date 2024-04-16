42d94f6a95a5824bc01350937282f1565078fcf1
Kelvin Rugut <krugut36@gmail.com>
2024-04-16 17:27:16 +0300

    Updates on dashboard

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -209,7 +209,7 @@
 				  </ul>
 				</li>
 				<li class="header">Components</li>
-				<li class="treeview">
+				<!-- <li class="treeview">
 				  <a href="#">
 					<i class="icon-Library"><span class="path1"></span><span class="path2"></span></i>
 					<span>Features</span>
@@ -306,8 +306,8 @@
 						</ul>
 					</li>  
 				  </ul>
-				</li>			
-				<li class="treeview">
+				</li>			 -->
+				<!-- <li class="treeview">
 				  <a href="#">
 					<i class="icon-Box2"><span class="path1"></span><span class="path2"></span></i>
 					<span>Forms, Tables & Charts</span>
@@ -492,7 +492,7 @@
 						</ul>
 					</li>
 				  </ul>
-				</li>	 
+				</li>	  -->
 				<li class="treeview">
 				  <a href="#">
 					<i class="icon-Lock-overturning"><span class="path1"></span><span class="path2"></span></i>
@@ -508,7 +508,7 @@
 					<li><a href="auth_user_pass.html"><i class="icon-Commit"><span class="path1"></span><span class="path2"></span></i>Recover password</a></li>	
 				  </ul>
 				</li>
-				<li class="treeview">
+				<!-- <li class="treeview">
 				  <a href="#">
 					<i class="icon-Warning-2"><span class="path1"></span><span class="path2"></span><span class="path3"></span></i>
 					<span>Miscellaneous</span>
@@ -521,7 +521,7 @@
 					<li><a href="error_500.html"><i class="icon-Commit"><span class="path1"></span><span class="path2"></span></i>Error 500</a></li>
 					<li><a href="error_maintenance.html"><i class="icon-Commit"><span class="path1"></span><span class="path2"></span></i>Maintenance</a></li>	
 				  </ul>
-				</li>	 	     
+				</li>	 	      -->
 			  </ul>
 			  
 			  <div class="sidebar-widgets">
@@ -946,7 +946,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-xl-4 col-12"> 
+				<!-- <div class="col-xl-4 col-12"> 
 					<div class="box">
 						<div class="box-header with-border">
 							<h3 class="box-title">New Members</h3>
@@ -1003,7 +1003,7 @@
 							</div>
 						</div>
 					</div>
-				</div>
+				</div> -->
 			</div>				
 		</section>
 		<!-- /.content -->
@@ -1016,12 +1016,9 @@
 		  <li class="nav-item">
 			<a class="nav-link" href="javascript:void(0)">FAQ</a>
 		  </li>
-		  <li class="nav-item">
-			<a class="nav-link" href="https://themeforest.net/item/lawfirm-responsive-bootstrap-admin-template/32736939">Purchase Now</a>
-		  </li>
 		</ul>
     </div>
-	  &copy; <script>document.write(new Date().getFullYear())</script> <a href="https://www.multipurposethemes.com/">Multipurpose Themes</a>. All Rights Reserved.
+	  &copy; <script>document.write(new Date().getFullYear())</script> <a href="#">Lawfirm Company</a>. All Rights Reserved.
   </footer>
 
   <!-- Control Sidebar -->
@@ -1293,129 +1290,6 @@
   
 </div>
 <!-- ./wrapper -->
-	
-	<!-- ./side demo panel -->
-	<div class="sticky-toolbar">	    
-	    <a href="https://themeforest.net/item/lawfirm-responsive-bootstrap-admin-template/32736939" data-bs-toggle="tooltip" data-bs-placement="left" title="Buy Now" class="waves-effect waves-light btn btn-success btn-flat mb-5 btn-sm" target="_blank">
-			<span class="icon-Money"><span class="path1"></span><span class="path2"></span></span>
-		</a>
-	    <a href="https://themeforest.net/user/multipurposethemes/portfolio" data-bs-toggle="tooltip" data-bs-placement="left" title="Portfolio" class="waves-effect waves-light btn btn-danger btn-flat mb-5 btn-sm" target="_blank">
-			<span class="icon-Image"></span>
-		</a>
-	    <a id="chat-popup" href="#" data-bs-toggle="tooltip" data-bs-placement="left" title="Live Chat" class="waves-effect waves-light btn btn-warning btn-flat btn-sm">
-			<span class="icon-Group-chat"><span class="path1"></span><span class="path2"></span></span>
-		</a>
-	</div>
-	<!-- Sidebar -->
-		
-	<div id="chat-box-body">
-		<div id="chat-circle" class="waves-effect waves-circle btn btn-circle btn-sm btn-warning l-h-50">
-            <div id="chat-overlay"></div>
-            <span class="icon-Group-chat fs-18"><span class="path1"></span><span class="path2"></span></span>
-		</div>
-
-		<div class="chat-box">
-            <div class="chat-box-header p-15 d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button class="waves-effect waves-circle btn btn-circle btn-primary-light h-40 w-40 rounded-circle l-h-45" type="button" data-bs-toggle="dropdown">
-                      <span class="icon-Add-user fs-22"><span class="path1"></span><span class="path2"></span></span>
-                  </button>
-                  <div class="dropdown-menu min-w-200">
-                    <a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Color me-15"></span>
-                        New Group</a>
-                    <a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Clipboard me-15"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span></span>
-                        Contacts</a>
-                    <a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Group me-15"><span class="path1"></span><span class="path2"></span></span>
-                        Groups</a>
-                    <a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Active-call me-15"><span class="path1"></span><span class="path2"></span></span>
-                        Calls</a>
-                    <a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Settings1 me-15"><span class="path1"></span><span class="path2"></span></span>
-                        Settings</a>
-                    <div class="dropdown-divider"></div>
-					<a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Question-circle me-15"><span class="path1"></span><span class="path2"></span></span>
-                        Help</a>
-					<a class="dropdown-item fs-16" href="#">
-                        <span class="icon-Notifications me-15"><span class="path1"></span><span class="path2"></span></span> 
-                        Privacy</a>
-                  </div>
-                </div>
-                <div class="text-center flex-grow-1">
-                    <div class="text-dark fs-18">Mayra Sibley</div>
-                    <div>
-                        <span class="badge badge-sm badge-dot badge-primary"></span>
-                        <span class="text-muted fs-12">Active</span>
-                    </div>
-                </div>
-                <div class="chat-box-toggle">
-                    <button id="chat-box-toggle" class="waves-effect waves-circle btn btn-circle btn-danger-light h-40 w-40 rounded-circle l-h-45" type="button">
-                      <span class="icon-Close fs-22"><span class="path1"></span><span class="path2"></span></span>
-                    </button>                    
-                </div>
-            </div>
-            <div class="chat-box-body">
-                <div class="chat-box-overlay">   
-                </div>
-                <div class="chat-logs">
-                    <div class="chat-msg user">
-                        <div class="d-flex align-items-center">
-                            <span class="msg-avatar">
-                                <img src="../images/avatar/2.jpg" class="avatar avatar-lg" alt="">
-                            </span>
-                            <div class="mx-10">
-                                <a href="#" class="text-dark hover-primary fw-bold">Mayra Sibley</a>
-                                <p class="text-muted fs-12 mb-0">2 Hours</p>
-                            </div>
-                        </div>
-                        <div class="cm-msg-text">
-                            Hi there, I'm Jesse and you?
-                        </div>
-                    </div>
-                    <div class="chat-msg self">
-                        <div class="d-flex align-items-center justify-content-end">
-                            <div class="mx-10">
-                                <a href="#" class="text-dark hover-primary fw-bold">You</a>
-                                <p class="text-muted fs-12 mb-0">3 minutes</p>
-                            </div>
-                            <span class="msg-avatar">
-                                <img src="../images/avatar/3.jpg" class="avatar avatar-lg" alt="">
-                            </span>
-                        </div>
-                        <div class="cm-msg-text">
-                           My name is Anne Clarc.         
-                        </div>        
-                    </div>
-                    <div class="chat-msg user">
-                        <div class="d-flex align-items-center">
-                            <span class="msg-avatar">
-                                <img src="../images/avatar/2.jpg" class="avatar avatar-lg" alt="">
-                            </span>
-                            <div class="mx-10">
-                                <a href="#" class="text-dark hover-primary fw-bold">Mayra Sibley</a>
-                                <p class="text-muted fs-12 mb-0">40 seconds</p>
-                            </div>
-                        </div>
-                        <div class="cm-msg-text">
-                            Nice to meet you Anne.<br>How can i help you?
-                        </div>
-                    </div>
-                </div><!--chat-log -->
-            </div>
-            <div class="chat-input">      
-                <form>
-                    <input type="text" id="chat-input" placeholder="Send a message..."/>
-                    <button type="submit" class="chat-submit" id="chat-submit">
-                        <span class="icon-Send fs-22"></span>
-                    </button>
-                </form>      
-            </div>
-		</div>
-	</div>
 	
 	<!-- Page Content overlay -->
 	
